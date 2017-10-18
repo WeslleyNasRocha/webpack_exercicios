a80928c7bcc3148a34cfa8f24985aeba28d47440
Weslley Rocha <weslleytato.12@gmail.com>
2017-10-18 16:08:24 -0200

    rest-spread

--- ex/index.js
+++ ex/index.js
@@ -1,4 +1,14 @@
-import Pessoa from './pessoa';
+const produto = {
+  nome: 'Caneta Bic Preta',
+  preco: 1.9,
+  desconto: 0.05
+};
 
-const pessoa = new Pessoa('Weslley');
-console.log(pessoa.toString());
+function clone(objeto) {
+  return { ...objeto };
+}
+
+const novoProduto = clone(produto);
+novoProduto.nome = 'Caneta Bic Azul';
+
+console.log(produto, novoProduto);
